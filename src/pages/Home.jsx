1808c43a2243ab65860eafbf5eac40e862524fcf
OxBryte <silascyrax@gmail.com>
2025-05-25 14:15:28 -0700

    feat: integrate Drawer component for creating new workspaces in AllWorkspace

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,12 @@ import AllWorkspace from "../components/features/AllWorkspace";
 
 export default function Home() {
   return (
-    <div className="w-full space-y-5">
+    <div className="w-full space-y-10">
       <div className="w-full flex flex-col md:flex-row gap-6">
         <div className="w-full h-[300px] rounded-lg bg-white border border-gray-300"></div>
         <div className="w-full h-[300px] rounded-lg bg-white border border-gray-300"></div>
       </div>
-      <div className="space-y-4 w-full">
-        <div className="flex items-center justify-between gap-3">
-          <p className="font-semibold text-[24px]">Workspace</p>
-        </div>
-        <AllWorkspace />
-      </div>
+      <AllWorkspace />
       <div className="space-y-3 w-full"></div>
     </div>
   );
--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -4,16 +4,7 @@ import AllWorkspace from "../components/features/AllWorkspace";
 export default function Workspace() {
   return (
     <div className="w-full space-y-5">
-      <div className="space-y-4 w-full">
-        <div className="flex items-center justify-between gap-3">
-          {/* <BsGrid3X2Gap size={30} className="text-gray-400" /> */}
-          <p className="font-semibold text-[24px]">Workspace</p>
-          <button className="bg-c-bg-2 px-5 py-3 rounded-lg text-sm font-medium bg-c-color transition-colors">
-            Create New Workspace
-          </button>
-        </div>
-        <AllWorkspace />
-      </div>
+      <AllWorkspace />
     </div>
   );
 }
