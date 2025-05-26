566944cf4dcf49f38ec982f1ee802afb2e2f6554
OxBryte <silascyrax@gmail.com>
2025-05-26 10:59:37 -0700

    feat: update Navbar to display dynamic user information; replace static welcome message and user details with values from auth context

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../hooks/auth";
 
 export default function Navbar() {
-
-  // const { user, isAuthenticated } = useAuth();
+  const { user } = useAuth();
 
   return (
     <div className="w-full mx-auto py-5 border-b border-black/10 h-[85.12px]">
       <div className="mx-auto px-4 md:px-6 flex items-center justify-between max-w-[1280px]">
-        <p className="font-semibold text-[20px]">Welcome, Bright Team</p>
+        <p className="font-semibold text-[20px]">
+          Welcome,{" "}
+          <span className="text-c-color capitalize">{user.username}</span>
+        </p>
         {/* <div className="flex items-center gap-3 h-auto">
           <Link to="/signup">
             <button className="px-5 py-2.5 bg-c-color rounded-lg hover:bg-c-bg cursor-pointer text-white">
@@ -26,8 +28,8 @@ export default function Navbar() {
         <div className="flex items-center gap-3">
           <div className="w-10 h-10 rounded-full bg-c-color"></div>
           <div className="space-y-0">
-            <p className="text-sm font-semibold">Olumide Silas</p>
-            <p className="text-xs text-gray-500">Admin</p>
+            <p className="text-sm font-semibold capitalize">{user.fullName}</p>
+            <p className="text-xs text-gray-500 capitalize">{user.role}</p>
           </div>
           <CgChevronDown />
         </div>
