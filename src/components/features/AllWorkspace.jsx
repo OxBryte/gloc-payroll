87b606898bc246517b2942bedeff34012be8dbc6
OxBryte <silascyrax@gmail.com>
2025-05-26 11:32:29 -0700

    feat: add loading state to AllWorkspace component; integrate isLoadingWorkspace for improved user experience during data fetching

--- src/components/features/AllWorkspace.jsx
+++ src/components/features/AllWorkspace.jsx
@@ -7,7 +7,7 @@ import moment from "moment";
 
 export default function AllWorkspace() {
   const [isOpen, setIsOpen] = useState(false);
-  const { workspace } = useGetWorkspace();
+  const { workspace, isLoadingWorkspace } = useGetWorkspace();
   // console.log(workspace);
 
   return (
@@ -22,59 +22,66 @@ export default function AllWorkspace() {
             Create New Workspace
           </button>
         </div>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {workspace?.length === 0 && (
-            <div className="w-full h-[300px] bg-white rounded-lg flex flex-col items-center justify-center gap-4 p-6 cursor-pointer hover:shadow-lg transition-shadow duration-200 ease-in-out">
-              <div className="w-12 h-12 rounded-lg bg-gray-200"></div>
-              <p className="text-gray-500 text-sm font-light">
-                No workspace yet!
-              </p>
-              <button
-                className="bg-c-color hover:bg-c-bg px-6 py-2.5 text-white rounded-lg cursor-pointer"
-                onClick={() => setIsOpen(true)}
-              >
-                Create Workspace
-              </button>
-            </div>
-          )}
-          {workspace?.map((space, index) => (
-            <Link to="/workspace/bright-team/overview" key={index}>
-              <div className="w-full h-[300px] bg-white rounded-lg flex flex-col gap-4 p-6 cursor-pointer hover:shadow-lg transition-shadow duration-200 ease-in-out">
-                <div className="flex w-full items-center justify-between gap-5">
-                  <div className="w-12 h-12 rounded-lg bg-gray-200"></div>
-                  <button className="">
-                    <GoKebabHorizontal size={26} />
-                  </button>
-                </div>
-                <div className="space-y-2">
-                  <p className="text-[20px] font-semibold">{space.name}</p>
-                  <p className="text-gray-500 text-sm font-light">
-                    {space.description}
-                  </p>
-                </div>
-                <hr className="border-black/10" />
-                <div className="space-y-2">
-                  <p className="font-light text-xs">Members/Staffs</p>
-                  {space.admins.length === 0 && (
-                    <p className="text-sm font-light">No admins yet</p>
-                  )}
-                  {/* <div className="flex items-center">
+        {isLoadingWorkspace ? (
+          <div className="w-full min-h-[320px] p-6 flex gap-3 flex-col items-center justify-center bg-white">
+            <img src="loading.svg" alt="" className="w-30" />
+            <p className="text-sm font-light">Loading workspace...</p>
+          </div>
+        ) : (
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {workspace?.length === 0 && (
+              <div className="w-full h-[320px] bg-white rounded-lg flex flex-col items-center justify-center gap-4 p-6 cursor-pointer hover:shadow-lg transition-shadow duration-200 ease-in-out">
+                <div className="w-12 h-12 rounded-lg bg-gray-200"></div>
+                <p className="text-gray-500 text-sm font-light">
+                  No workspace yet!
+                </p>
+                <button
+                  className="bg-c-color hover:bg-c-bg px-6 py-2.5 text-white rounded-lg cursor-pointer"
+                  onClick={() => setIsOpen(true)}
+                >
+                  Create Workspace
+                </button>
+              </div>
+            )}
+            {workspace?.map((space, index) => (
+              <Link to="/workspace/bright-team/overview" key={index}>
+                <div className="w-full h-[320px] bg-white rounded-lg flex flex-col gap-4 p-6 cursor-pointer hover:shadow-lg transition-shadow duration-200 ease-in-out">
+                  <div className="flex w-full items-center justify-between gap-5">
+                    <div className="w-12 h-12 rounded-lg bg-gray-200"></div>
+                    <button className="">
+                      <GoKebabHorizontal size={26} />
+                    </button>
+                  </div>
+                  <div className="space-y-2">
+                    <p className="text-[20px] font-semibold">{space.name}</p>
+                    <p className="text-gray-500 text-sm font-light">
+                      {space.description}
+                    </p>
+                  </div>
+                  <hr className="border-black/10" />
+                  <div className="space-y-2">
+                    <p className="font-light text-xs">Members/Staffs</p>
+                    {space.admins.length === 0 && (
+                      <p className="text-sm font-light">No admins yet</p>
+                    )}
+                    {/* <div className="flex items-center">
                     <div className="w-8 h-8 rounded-full bg-gray-200"></div>
                     <div className="w-8 h-8 rounded-full bg-gray-200"></div>
                     <div className="w-8 h-8 rounded-full bg-gray-200"></div>
                     <div className="w-8 h-8 rounded-full bg-gray-200"></div>
                     <p className="text-gray-500 text-sm">+2 more</p>
-                  </div> */}
+                    </div> */}
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <p className="text-gray-500 text-xs">
+                      Created at: {moment(space.cratedAt).format("LLL")}
+                    </p>
+                  </div>
                 </div>
-                <div className="flex items-center justify-between">
-                  <p className="text-gray-500 text-xs">
-                    Created at: {moment(space.cratedAt).format("LLL")}
-                  </p>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
       {isOpen && <Drawer setIsOpen={setIsOpen} />}
     </>
