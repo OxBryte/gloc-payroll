9fdfde86918d2d9a95a095743ec16ce42f91b59f
OxBryte <silascyrax@gmail.com>
2025-06-07 15:37:36 -0700

    feat: enhance Account component with user data display and styling improvements

--- src/components/features/settings/Account.jsx
+++ src/components/features/settings/Account.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { useUser } from "../../hooks/useUser";
 
 export default function Account() {
+  const { user, isLoadingUser } = useUser();
+
   return (
     <div className="w-full space-y-6">
       <div className="space-y-3">
-        <p className="">Avatar</p>
-        <p>Edit your profile picture</p>
+        <p className="font-semibold text-lg">Avatar</p>
+        <p className="text-xs font-light">Edit your profile picture</p>
+        <img
+          src={user?.avatar}
+          alt={user?.fullName}
+          className="w-20 h-20 overflow-hidden rounded-full object-cover"
+        />
+      </div>
+      <hr className="border-black/10" />
+      <div className="space-y-3">
+        <p className="font-semibold text-lg">Personal Information</p>
+        <p className="text-xs font-light">Edit your personal information</p>
       </div>
     </div>
   );
