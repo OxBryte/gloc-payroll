0fefcababe25999c383d5d56429ae794cc1d4b2f
OxBryte <silascyrax@gmail.com>
2025-07-04 10:00:18 -0700

    feat: add network-speed package and integrate NetworkStatusBanner component in AppLayout for improved network status display

--- src/components/layouts/AppLayout.jsx
+++ src/components/layouts/AppLayout.jsx
@@ -3,10 +3,12 @@ import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import WorkspaceLayout from "./WorkspaceLayout";
+import NetworkStatusBanner from "../ui/NetworkStatusBanner";
 
 export default function AppLayout() {
   return (
     <div className="w-full flex gap-0 relative h-full">
+      <NetworkStatusBanner />
       <Sidebar />
       {/* <WorkspaceLayout /> */}
       <div className="space-y-0 w-full relative">
