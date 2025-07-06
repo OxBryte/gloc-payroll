5da1ea70a11a7895669ab2b760fc93ff18b572ab
OxBryte <silascyrax@gmail.com>
2025-07-06 16:43:43 -0700

    auto-commit: 2025-07-06 23:43:43

---
+++ src/components/features/job/JobCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function JobCard() {
+  return (
+      <div className='w-full rounded-full border border-gray-100 '>
+          
+    </div>
+  )
+}
--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import JobCard from "../components/features/job/JobCard";
 
 export default function Jobs() {
   return (
-    <div className="w-full h-[70dvh] flex flex-col items-center justify-center gap-4 bg-white p-4 rounded-lg">
+    <>
+      {/* <div className="w-full h-[70dvh] flex flex-col items-center justify-center gap-4 bg-white p-4 rounded-lg">
       <img src="/jobs.svg" alt="Jobs Illustration" className="w-50" />
       <p className="font-light max-w-[340px] text-center">
         No jobs available yet. This section is under development (Coming Soon).
       </p>
-    </div>
+    </div> */}
+      <div className="w-full h-[70dvh] gap-4 bg-white p-4 rounded-lg">
+        <JobCard />
+      </div>
+    </>
   );
 }
