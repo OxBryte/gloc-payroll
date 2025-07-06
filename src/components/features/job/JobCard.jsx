0671e06ebc2044f219a1a6701c36bf7d8088bcba
OxBryte <silascyrax@gmail.com>
2025-07-06 16:55:43 -0700

    auto-commit: 2025-07-06 23:55:43

--- src/components/features/job/JobCard.jsx
+++ src/components/features/job/JobCard.jsx
@@ -4,12 +4,14 @@ export default function JobCard() {
   return (
     <div className="w-full max-w-[280px] rounded-lg border border-gray-100 p-4 flex flex-col gap-3">
       <div className="flex w-full gap-3 items-center">
-        <div className="w-20 h-20 rounded-lg bg-[#e9e9e9]"></div>
-              <div className="space-y-2">
-                  <p className="font-semibold text-xl">Role Title</p>
-                  <p className="font-light text-sm">Company Name</p>
-      </div>
+        <div className="w-12 h-12 rounded-lg bg-[#e9e9e9]"></div>
+        <div className="space-y-1">
+          <p className="font-semibold">Company name</p>
+          <p className="font-light text-xs">Company Location</p>
+        </div>
           </div>
+          <p className="font-semibold text-lg">Front-end Developer</p>
+          <span className="bg-[]"></span>
     </div>
   );
 }
