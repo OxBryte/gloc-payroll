82e82f7e2749c8c78612a2aa82be878908cc9b16
OxBryte <silascyrax@gmail.com>
2025-07-16 23:37:08 +0100

    feat: update 2025-07-16 22:37:08

--- src/components/features/workspace/Jobs.jsx
+++ src/components/features/workspace/Jobs.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Jobs() {
   return (
-    <div>Jobs</div>
+      <div className='w-full space-y-5'>
+          <div className="w-full bg-gradient-to-r"></div>
+    </div>
   )
 }
