09470d18f2081bfbd00946ba7af28ebd5043e4ec
OxBryte <silascyrax@gmail.com>
2025-07-16 23:50:08 +0100

    feat: update 2025-07-16 22:50:08

--- src/components/features/workspace/Jobs.jsx
+++ src/components/features/workspace/Jobs.jsx
@@ -14,10 +14,10 @@ export default function Jobs() {
         </p>
         <button className="bg-c-color text-white px-6 py-2.5 text-sm rounded-lg cursor-pointer hover:bg-c-bg transition-colors duration-200">
           Create a Job
-              </button>
-              <div className="absolute -bottom-10 opacity-[10%] right-0 ">
-                  <img src="/gloc-border.svg" alt="" className="w-60" />
-              </div>
+        </button>
+        <div className="absolute -bottom-10 opacity-[20%] right-0 ">
+          <img src="/gloc-border.svg" alt="" className="w-60" />
+        </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2  gap-4 w-full">
