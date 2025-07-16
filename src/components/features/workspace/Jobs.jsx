b2c27f5a4a214e00d66f11dcc86eefcee4491c90
OxBryte <silascyrax@gmail.com>
2025-07-16 23:40:08 +0100

    feat: update 2025-07-16 22:40:08

--- src/components/features/workspace/Jobs.jsx
+++ src/components/features/workspace/Jobs.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Jobs() {
   return (
     <div className="w-full space-y-5">
-          <div className="w-full bg-gradient-to-r from-c-color via-black to-c-color-sec rounded-lg p-4">
-              Here is a Job Board for your Project
+      <div className="w-full bg-gradient-to-r from-c-color via-black to-c-color-sec rounded-lg p-4">
+        <h1 className="font-semibold text-white font-[24px]">
+          {" "}
+          Here is a Job Board for your Project
+        </h1>
+        <p>Connect your software with the curring-edge technology.</p>
+        <button className="bg-c-color text-white px-6 py-2.5 text-sm rounded-lg cursor-pointer hover:bg-c-bg transition-colors duration-200">
+          Create Job
+        </button>
       </div>
     </div>
   );
