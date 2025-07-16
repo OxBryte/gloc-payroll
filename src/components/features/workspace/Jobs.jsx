6fb5b3b20fa3d509e464736b80075517ed7bb7ee
OxBryte <silascyrax@gmail.com>
2025-07-16 23:46:08 +0100

    feat: update 2025-07-16 22:46:08

--- src/components/features/workspace/Jobs.jsx
+++ src/components/features/workspace/Jobs.jsx
@@ -4,7 +4,7 @@ import JobCard from "../job/JobCard";
 export default function Jobs() {
   return (
     <div className="w-full space-y-5">
-      <div className="w-full bg-gradient-to-r from-black to-c-color rounded-lg p-10 space-y-2">
+      <div className="relative w-full bg-gradient-to-r from-black to-c-color rounded-lg p-10 space-y-2">
         <h1 className="font-bold text-white text-[24px]">
           {" "}
           Here is a Job Board for your Project
@@ -15,8 +15,8 @@ export default function Jobs() {
         <button className="bg-c-color text-white px-6 py-2.5 text-sm rounded-lg cursor-pointer hover:bg-c-bg transition-colors duration-200">
           Create a Job
               </button>
-              <div className="absolute -bottom-20 right-0 ">
-                  <img src="/gloc-logo.svg" alt="" />
+              <div className="absolute -bottom-10 opacity-[10%] right-0 ">
+                  <img src="/gloc-logo-white.svg" alt="" className="w-40" />
               </div>
       </div>
 
