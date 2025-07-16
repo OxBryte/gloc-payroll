58f91cd5aa46804633288906ef1fcd133962b569
OxBryte <silascyrax@gmail.com>
2025-07-16 23:44:08 +0100

    feat: update 2025-07-16 22:44:08

--- src/components/features/workspace/Jobs.jsx
+++ src/components/features/workspace/Jobs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import JobCard from "../job/JobCard";
 
 export default function Jobs() {
   return (
@@ -14,9 +15,13 @@ export default function Jobs() {
         <button className="bg-c-color text-white px-6 py-2.5 text-sm rounded-lg cursor-pointer hover:bg-c-bg transition-colors duration-200">
           Create a Job
         </button>
-          </div>
-          
-          
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-4 w-full">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <JobCard key={index} />
+        ))}
+      </div>
     </div>
   );
 }
