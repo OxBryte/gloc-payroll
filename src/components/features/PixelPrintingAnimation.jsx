003a35f1a2a46680c33e5578aa6f5c9e83fe592b
OxBryte <silascyrax@gmail.com>
2025-07-17 02:52:09 +0100

    feat: update 2025-07-17 01:52:08

--- src/components/features/PixelPrintingAnimation.jsx
+++ src/components/features/PixelPrintingAnimation.jsx
@@ -3,7 +3,7 @@ import * as THREE from "three";
 import gsap from "gsap";
 
 const PixelPrintingAnimation = ({
-  svgPath = "/gloc-logo.svg",
+  svgPath = "/west-africa.svg",
   dotSize = 2,
   dotSpacing = 4,
   dotColor = 0x00ff88,
