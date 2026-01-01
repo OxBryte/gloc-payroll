dd08c1c5894344656947039577f3cf9609f01248
OxBryte <silascyrax@gmail.com>
2026-01-01 17:06:18 +0100

    feat: update

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -75,6 +75,7 @@ export default function Tasks() {
         {/* Tabs */}
         <div className="mb-6 border-b border-gray-200">
           <div className="flex gap-1">
+            
             {["opened", "closed", "archived"].map((tab) => (
               <button
                 key={tab}
