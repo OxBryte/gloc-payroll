2d068ae161fc68ea6f2b885ceea22a2d7a364744
OxBryte <silascyrax@gmail.com>
2026-01-01 17:07:56 +0100

    feat: update

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -96,7 +96,7 @@ export default function Tasks() {
                   ${
                     activeTab === tab
                       ? "text-c-color"
-                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                      : "border-transparent text-gray-400 hover:text-gray-700 hover:border-gray-300"
                   }
                 `}
               >
