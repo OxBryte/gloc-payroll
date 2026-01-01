30504aebd635b553c353238e290280036efea211
OxBryte <silascyrax@gmail.com>
2026-01-01 15:03:52 +0100

    feat: update

--- src/components/ui/EditTaskModal.jsx
+++ src/components/ui/EditTaskModal.jsx
@@ -26,22 +26,14 @@ export default function EditTaskModal({ task, setIsOpen }) {
         }
       };
 
-      const icon = task.icon || "CheckCircle2";
-      setSelectedIcon(icon);
       reset({
         title: task.title || "",
         description: task.description || "",
-        icon: icon,
         startDate: formatDateForInput(task.startDate),
         completionDate: formatDateForInput(task.completionDate),
       });
     }
   }, [task, reset]);
-
-  const handleIconSelect = (iconName) => {
-    setSelectedIcon(iconName);
-    setValue("icon", iconName);
-  };
 
   const onSubmit = async (data) => {
     try {
