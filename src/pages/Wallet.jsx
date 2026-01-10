cae06499b543a24380b87b786fe2e45519c49e99
OxBryte <silascyrax@gmail.com>
2026-01-10 20:39:00 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -11,12 +11,6 @@ const Wallet = () => {
   return (
     <div className="min-h-screen max-w-[760px] mx-auto bg-gray-50 flex justify-center ">
       <div className="w-full  min-h-screen relative flex flex-col">
-        {/* Content Area */}
-        <div className="flex-1 flex flex-col items-center justify-center p-6 pb-24">
-          <div className="w-full max-w-sm space-y-8 text-center"></div>
-        </div>
-
-        {/* Fixed Bottom Navigation Bar */}
         <BottomBar />
       </div>
     </div>
