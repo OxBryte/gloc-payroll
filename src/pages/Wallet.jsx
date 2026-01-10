0a3d169d0ef46a97ff8febff230b3e6f9c372b3c
OxBryte <silascyrax@gmail.com>
2026-01-10 20:59:10 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -10,8 +10,10 @@ const Wallet = () => {
   return (
     <div className="min-h-screen  w-fullflex justify-center relative">
       <div className="w-full border-b border-b-gray-200 p-4">
-        <div className="flex items-center justify-between"></div>
+        <div className="flex items-center justify-between">
+
         <img src="/gloc-logo.svg" alt="logo" className="w-8" />
+        </div>
         {/* <div className="w-10 h-10 rounded-full bg-gradient-to-br from-[#0f0f0f] to-c-color-sec cursor-pointer hover:opacity-90 transition-opacity shadow-sm"></div> */}
       </div>
       <div className="w-full max-w-[760px] mx-auto min-h-screen relative flex flex-col"></div>
