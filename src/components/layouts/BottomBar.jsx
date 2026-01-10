2e7f4784e8b381b3d1541021fecc485f51456a8e
OxBryte <silascyrax@gmail.com>
2026-01-10 20:45:23 +0100

    feat: update

--- src/components/layouts/BottomBar.jsx
+++ src/components/layouts/BottomBar.jsx
@@ -25,7 +25,7 @@ export default function BottomBar() {
                 }`}
               >
                       <item.icon className={`w-6 h-6 ${item.active ? "" : ""}`} />
-                      <span> </span>
+                      <span> {item.label}</span>
               </div>
             </button>
           ))}
