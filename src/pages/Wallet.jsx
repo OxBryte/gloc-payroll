a0cccfee9c1c41b7a63bd0aa33d8cffcc05a8912
OxBryte <silascyrax@gmail.com>
2026-01-10 20:53:20 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -8,7 +8,7 @@ const Wallet = () => {
   const { address, isConnected } = useAppKitAccount();
 
   return (
-    <div className="min-h-screen  mx-auto bg-gray-50 flex justify-center relative">
+    <div className="min-h-screen max-w-[760px] mx-auto bg-gray-50 flex justify-center relative">
       <div className="w-full  min-h-screen relative flex flex-col"></div>
       <BottomBar />
     </div>
