ff77b431746755f3ed0c07eee6b97eb00a344cb8
OxBryte <silascyrax@gmail.com>
2026-01-10 20:46:49 +0100

    feat: update

--- src/components/layouts/BottomBar.jsx
+++ src/components/layouts/BottomBar.jsx
@@ -27,6 +27,7 @@ export default function BottomBar() {
                 <item.icon className={`w-6 h-6 ${item.active ? "" : ""}`} />{" "}
                 {item.active && (
                   <span className="w-2 h-2 bg-green-500 rounded-full">
+                    {item.label}
                   </span>
                 )}
               </div>
