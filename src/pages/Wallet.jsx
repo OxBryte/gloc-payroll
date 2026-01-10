28461e2647fcddfe325d96d81f66d145078f7a60
OxBryte <silascyrax@gmail.com>
2026-01-10 20:39:14 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
-import { Wallet as WalletIcon, BookUser, ShieldCheck } from "lucide-react";
 import { truncateAddress } from "../components/lib/utils";
 import BottomBar from "../components/layouts/BottomBar";
 
