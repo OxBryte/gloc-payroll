af24b06d9bd0955ae26de8d6ae20efba8733a949
OxBryte <silascyrax@gmail.com>
2026-01-10 20:38:21 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -10,7 +10,7 @@ const Wallet = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 flex justify-center ">
-      <div className="w-full max-w-[760px] min-h-screen relative flex flex-col">
+      <div className="w-full  min-h-screen relative flex flex-col">
         {/* Content Area */}
         <div className="flex-1 flex flex-col items-center justify-center p-6 pb-24">
           <div className="w-full max-w-sm space-y-8 text-center">
