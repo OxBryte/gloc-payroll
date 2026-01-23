0f1940a98c645f22ef4f436cc24b76bff4b9fb5c
OxBryte <silascyrax@gmail.com>
2026-01-23 23:04:23 +0100

    feat: update

--- src/components/ui/EditWalletModal.jsx
+++ src/components/ui/EditWalletModal.jsx
@@ -149,16 +149,7 @@ const EditWalletModal = ({ isOpen, onClose, wallet, onSave }) => {
                 )}
               </div>
               
-              {/* Image Upload Button */}
-              <div className="relative mt-2">
-                <button
-                  onClick={() => fileInputRef.current?.click()}
-                  className="w-8 h-8 rounded-full bg-gray-200 hover:bg-gray-300 flex items-center justify-center transition-colors"
-                >
-                  <MoreHorizontal className="w-4 h-4 text-gray-600" />
-                </button>
-               
-              </div>
+             
             </div>
 
             {/* Background Gradient Picker */}
