0d145550b16143f4172833829895b585f6d87c23
OxBryte <silascyrax@gmail.com>
2026-01-23 18:46:56 +0100

    feat: update

--- src/components/ui/WalletMenu.jsx
+++ src/components/ui/WalletMenu.jsx
@@ -58,7 +58,7 @@ const WalletMenu = ({ onOptionSelect }) => {
           </button>
           <button
             onClick={() => handleMenuOption("transactions")}
-            className="w-full text-left px-4 py-2.5 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
+            className="w-full text-left px-2.5 py-1.5 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
           >
             Transactions
           </button>
