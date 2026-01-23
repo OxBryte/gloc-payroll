a20ad0431da6ac17ac2bdbc82c3471cbcca10eb8
OxBryte <silascyrax@gmail.com>
2026-01-23 23:24:06 +0100

    feat: update

--- src/components/ui/QRCodeModal.jsx
+++ src/components/ui/QRCodeModal.jsx
@@ -30,7 +30,7 @@ const QRCodeModal = ({ isOpen, onClose, wallet }) => {
       />
 
       {/* Modal */}
-      <div className="relative w-full max-w-md mx-auto overflow-hidden ">
+      <div className="relative w-full max-w-md mx-auto overflow-hidden space-y-6 l">
         {/* Content */}
 
         {/* Wallet Info */}
