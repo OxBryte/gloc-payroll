fa403b7bd00374f953624b1eb84279bf4af0b143
OxBryte <silascyrax@gmail.com>
2026-01-23 23:29:40 +0100

    feat: update

--- src/components/ui/QRCodeModal.jsx
+++ src/components/ui/QRCodeModal.jsx
@@ -49,7 +49,7 @@ const QRCodeModal = ({ isOpen, onClose, wallet }) => {
             round={true}
             level="H"
             bgColor="#ffffff"
-            marginSize={10}
+            marginSize={5}
           />
         </div>
         <button
