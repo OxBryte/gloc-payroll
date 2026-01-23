5a66a9f8b64a0d873ba2449ed0d2136e8c6ac1a6
OxBryte <silascyrax@gmail.com>
2026-01-23 18:49:20 +0100

    feat: update

--- src/components/ui/WalletMenu.jsx
+++ src/components/ui/WalletMenu.jsx
@@ -56,7 +56,7 @@ const WalletMenu = ({ onOptionSelect }) => {
             onClick={() => handleMenuOption("edit-picture")}
             className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
           >
-            
+            <LuUser size={14} />
             Edit Picture
           </button>
           <button
