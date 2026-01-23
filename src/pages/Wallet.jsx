6cbfce0764b089c7a3feb71522f80941318fb0c1
OxBryte <silascyrax@gmail.com>
2026-01-23 18:22:14 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -29,7 +29,7 @@ const Wallet = () => {
             </div>
             <div className="flex items-center gap-3">
               <p className="text-[18px] font-medium">$0.00</p>
-              <HiOutlineDotsVertical size={20} />
+              <HiOutlineDotsVertical size={20} className="text-gray-500 cursor-pointer" />
             </div>
           </div>
           <div className="border-b border-b-gray-100"></div>
