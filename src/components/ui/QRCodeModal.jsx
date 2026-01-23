e09e54e673c93d8417b2f05c960fbce8927a9a32
OxBryte <silascyrax@gmail.com>
2026-01-23 23:31:08 +0100

    feat: update

--- src/components/ui/QRCodeModal.jsx
+++ src/components/ui/QRCodeModal.jsx
@@ -42,7 +42,7 @@ const QRCodeModal = ({ isOpen, onClose, wallet }) => {
 
         {/* QR Code */}
 
-        <div className="flex items-center justify-center w-fit rounded-full overflow-hidden">
+        <div className="flex items-center justify-center w-fit rounded-10 overflow-hidden">
           <QRCodeSVG
             value={wallet.address}
             size={300}
