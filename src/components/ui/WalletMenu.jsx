4a61ceef0531bcd939e64090fbed5174c5bd2bbc
OxBryte <silascyrax@gmail.com>
2026-01-23 18:48:43 +0100

    feat: update

--- src/components/ui/WalletMenu.jsx
+++ src/components/ui/WalletMenu.jsx
@@ -47,7 +47,7 @@ const WalletMenu = ({ onOptionSelect }) => {
         <div className="absolute right-0 top-8 z-50 w-48 p-2 bg-c-bg text-white border border-gray-200 rounded-lg shadow-lg overflow-hidden">
           <button
             onClick={() => handleMenuOption("edit-name")}
-            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-[#1E1E1E] transition-colors"
+            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-[#1F1E1E] transition-colors"
           >
             <LuPen size={14} />
             Edit Name
