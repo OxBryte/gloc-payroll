48325a42f747b3d1599db16a06ce5add472cadd9
OxBryte <silascyrax@gmail.com>
2026-01-23 18:08:14 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -18,7 +18,7 @@ const Wallet = () => {
         <div className="w-full h-full bg-white rounded-lg border border-gray-100">
           <div className="w-full p-4 flex gap-3">
             <div className="flex gap-2 items-center">
-            <div className="w-10 h-10 rounded-full bg-c-color"></div>
+              <div className="w-10 h-10 rounded-full bg-c-color"></div>
               <div className="space-y-2">
                 <p className="text-sm font-medium">John Doe</p>
                 <p className="text-xs text-gray-500">0x1234567890</p>
