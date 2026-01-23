cf53ce7f35fdda03b5be5c0bd7af8f0f300c9636
OxBryte <silascyrax@gmail.com>
2026-01-23 23:22:16 +0100

    feat: update

--- src/components/ui/QRCodeModal.jsx
+++ src/components/ui/QRCodeModal.jsx
@@ -58,7 +58,7 @@ const QRCodeModal = ({ isOpen, onClose, wallet }) => {
             size={200}
             level="H"
             bgColor="#ffffff"
-marh
+includeMargin={true}
           />
 
           {/* Address with Copy Button */}
