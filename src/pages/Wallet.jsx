ebff3ed93f32bfecda7e24d3cc52a17d871c3a2c
OxBryte <silascyrax@gmail.com>
2026-01-23 18:26:37 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -62,9 +62,6 @@ const Wallet = () => {
             <HiOutlinePlus size={20} className="" />
             <p>Create New Wallet</p>
           </div>
-          <div className="w-full bg-c-color text-white rounded-[20px] p-4 flex items-center justify-center gap-3 cursor-pointer hover:bg-c-color/80 hover:scale-98 transition-all duration-300">
-            <HiOutlinePlus size={20} className="" />
-          </div>
             <p>Import Wallet</p>
         </div>
       </div>
