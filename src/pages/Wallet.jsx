9bd326f10d2c9ac945218815d7998ccdae292ad3
OxBryte <silascyrax@gmail.com>
2026-01-23 18:07:07 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -16,7 +16,7 @@ const Wallet = () => {
       </div>
       <div className="mt-10 w-full max-w-[560px] mx-auto relative">
         <div className="w-full h-full bg-white rounded-lg border border-gray-100">
-          <div className="w-full "></div>
+          <div className="w-full p-4"></div>
         </div>
       </div>
       <BottomBar />
