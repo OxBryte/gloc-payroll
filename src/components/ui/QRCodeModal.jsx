bdfa4f804ed59e1681c9b3829d212b8b4bf35f6f
OxBryte <silascyrax@gmail.com>
2026-01-23 23:32:06 +0100

    feat: update

--- src/components/ui/QRCodeModal.jsx
+++ src/components/ui/QRCodeModal.jsx
@@ -49,7 +49,7 @@ const QRCodeModal = ({ isOpen, onClose, wallet }) => {
             round={true}
             level="H"
             bgColor="#ffffff"
-            lever
+            level="L"
             marginSize={5}
             // imageSettings={{
             //   src: "/gloc-logo.svg",
