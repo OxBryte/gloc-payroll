7185259eb1c8527d35a567b62febec2768818b17
OxBryte <silascyrax@gmail.com>
2026-01-23 18:14:33 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -28,6 +28,7 @@ const Wallet = () => {
             </div>
             <div className="flex items-center gap-3">
               <p className="text-[18px] font-medium">$0.00</p>
+              
             </div>
           </div>
           <div className="border-b border-b-gray-100"></div>
