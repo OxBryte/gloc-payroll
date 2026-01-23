dced2435eeb8085bc4e4619ffc050ea04fa2f170
OxBryte <silascyrax@gmail.com>
2026-01-23 18:01:00 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
-import { truncateAddress } from "../components/lib/utils";
+// import { truncateAddress } from "../components/lib/utils";
 import BottomBar from "../components/layouts/BottomBar";
 
 const Wallet = () => {
-  const { open } = useAppKit();
-  const { address, isConnected } = useAppKitAccount();
+  // const { open } = useAppKit();
+  // const { address, isConnected } = useAppKitAccount();
 
   return (
     <div className="min-h-screen w-full relative">
