18ccff743b60aff4ed9238ebb57a82d008d0b532
OxBryte <silascyrax@gmail.com>
2026-01-23 18:47:14 +0100

    feat: update

--- src/components/ui/WalletMenu.jsx
+++ src/components/ui/WalletMenu.jsx
@@ -46,37 +46,37 @@ const WalletMenu = ({ onOptionSelect }) => {
         <div className="absolute right-0 top-8 z-50 w-48 p-2 bg-c-bg text-white border border-gray-200 rounded-lg shadow-lg overflow-hidden">
           <button
             onClick={() => handleMenuOption("edit-name")}
-            className="w-full text-left px-2.5 py-1.5 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
+            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
           >
             Edit Name
           </button>
           <button
             onClick={() => handleMenuOption("edit-picture")}
-            className="w-full text-left px-2.5 py-1.5 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
+            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
           >
             Edit Picture
           </button>
           <button
             onClick={() => handleMenuOption("transactions")}
-            className="w-full text-left px-2.5 py-1.5 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
+            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
           >
             Transactions
           </button>
           <button
             onClick={() => handleMenuOption("copy-address")}
-            className="w-full text-left px-2.5 py-1.5 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
+            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
           >
             Copy Address
           </button>
           <button
             onClick={() => handleMenuOption("qr-code")}
-            className="w-full text-left px-2.5 py-1.5 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
+            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
           >
             QR Code
           </button>
           <button
             onClick={() => handleMenuOption("export-wallet")}
-            className="w-full text-left px-2.5 py-1.5 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
+            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white hover:bg-c-color/80 transition-colors"
           >
             Export Wallet
           </button>
