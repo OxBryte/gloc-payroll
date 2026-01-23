f1df853a322ee8b2a879e84d3bb5e56151602556
OxBryte <silascyrax@gmail.com>
2026-01-23 18:23:02 +0100

    feat: update

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -29,7 +29,10 @@ const Wallet = () => {
             </div>
             <div className="flex items-center gap-3">
               <p className="text-[18px] font-medium">$0.00</p>
-              <HiOutlineDotsVertical size={20} className="text-gray-400 cursor-pointer" />
+              <HiOutlineDotsVertical
+                size={20}
+                className="text-gray-400 cursor-pointer"
+              />
             </div>
           </div>
           <div className="border-b border-b-gray-100"></div>
@@ -42,6 +45,13 @@ const Wallet = () => {
                   {truncateAddress(address)}
                 </p>
               </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <p className="text-[18px] font-medium">$0.00</p>
+              <HiOutlineDotsVertical
+                size={20}
+                className="text-gray-400 cursor-pointer"
+              />
             </div>
           </div>
         </div>
