5fab406546640a91dac1d3c5b35481f369d2c74e
OxBryte <silascyrax@gmail.com>
2026-01-24 14:35:53 +0100

    feat: update

--- src/components/ui/WalletMenu.jsx
+++ src/components/ui/WalletMenu.jsx
@@ -60,13 +60,7 @@ const WalletMenu = ({ onOptionSelect }) => {
             <LuPen size={14} />
             Edit Wallet
           </button>
-          <button
-            onClick={() => handleMenuOption("edit-picture")}
-            className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white/80 hover:bg-[#1F1F1F] hover:text-white rounded-lg cursor-pointer transition-colors cursor-pointer"
-          >
-            <LuSend size={14} />
-            Send
-          </button>
+        in
           <button
             onClick={() => handleMenuOption("transactions")}
             className="w-full text-left px-2.5 py-1.5 flex items-center gap-2 text-[14px] font-light text-white/80 hover:bg-[#1F1F1F] hover:text-white rounded-lg cursor-pointer transition-colors cursor-pointer"
